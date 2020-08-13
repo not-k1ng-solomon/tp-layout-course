58739fbf75e8b1a6ee1ae4250727e443d12ca1de
not-k1ng-solomon <useinov.sh.i14@gmail.com>
2020-08-13 23:15:03 +0300

    end slider

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,10 @@ const browserSync = require('browser-sync');
 const isDevelopment = true;
 const debugInfo = false;
 
-gulp.task('less',()=>{
+gulp.task('less', () => {
     return gulp.src('src/css/**/*.less')
-        .pipe(gulpIf(debugInfo,debug({title:'src'})))
-        .pipe(gulpIf(isDevelopment,sourcemaps.init()))
+        .pipe(gulpIf(debugInfo, debug({title: 'src'})))
+        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 16 versions'],
             cascade: false
@@ -25,14 +25,14 @@ gulp.task('less',()=>{
         .pipe(less())
         .pipe(concat('bundle.css'))
         // .pipe(cleanCss())
-        .pipe(gulpIf(isDevelopment,sourcemaps.write()))
+        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
         .pipe(gulp.dest('public/css'))
         .pipe(browserSync.stream());
 });
-gulp.task('sass',()=>{
+gulp.task('sass', () => {
     return gulp.src('src/css/**/*.scss')
-        .pipe(gulpIf(debugInfo,debug({title:'src'})))
-        .pipe(gulpIf(isDevelopment,sourcemaps.init()))
+        .pipe(gulpIf(debugInfo, debug({title: 'src'})))
+        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 16 versions'],
             cascade: false
@@ -40,15 +40,25 @@ gulp.task('sass',()=>{
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('style.css'))
         // .pipe(cleanCss())
-        .pipe(gulpIf(isDevelopment,sourcemaps.write()))
+        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
         .pipe(gulp.dest('public/css'))
         .pipe(browserSync.stream());
 });
-gulp.task('html',()=>{
+gulp.task('html', () => {
     return gulp.src('src/**/*.html')
         .pipe(gulp.dest('public'));
 });
-gulp.task('img',()=>{
+
+gulp.task('js', () => {
+    return gulp.src('src/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('public/js'));
+});
+gulp.task('php', () => {
+    return gulp.src('src/**/*.php')
+        .pipe(gulp.dest('public'));
+});
+gulp.task('img', () => {
     return gulp.src('src/img/**/*.+(png|jpg|jpeg|svg)')
         .pipe(imagemin([
             imagemin.gifsicle(),
@@ -58,31 +68,28 @@ gulp.task('img',()=>{
         .pipe(gulp.dest('public/img'));
 });
 
-gulp.task('serve',()=>{
+gulp.task('serve', () => {
     browserSync.init({
-        server:{
-            baseDir:'./public'
+        server: {
+            baseDir: './public'
         }
     });
-    gulp.watch('src/css/**/*.less',gulp.series('less'));
+    // gulp.watch('src/css/**/*.less', gulp.series('less'));
     gulp.watch('src/css/**/*.scss', gulp.series('sass'));
-    gulp.watch('src/*.html',gulp.series('html'));
-    gulp.watch('src/img/**/*.+(png|jpg|jpeg|svg)',gulp.series('img'));
-    gulp.watch('src/*.html').on('change',browserSync.reload);
-
+    gulp.watch('src/*.html', gulp.series('html'));
+    gulp.watch('src/*.php', gulp.series('php'));
+    gulp.watch('src/js/**/*.js', gulp.series('js'));
+    gulp.watch('src/img/**/*.+(png|jpg|jpeg|svg)', gulp.series('img'));
+    gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default',gulp.series('less','sass','html','img','serve'));
-
-//Урок 1
-/*
-gulp.task('css', () => {
-    return gulp.src('src/!**!/!*')
-        .pipe(gulp.dest('tmp'));
-});
-gulp.task('hello2', () => {
-    console.log('hello world2');
-});
-
-gulp.task('default',gulp.parallel('css'));
-*/
+gulp.task('default',
+    gulp.series(
+        // 'less',
+        'sass',
+        'html',
+        // 'img',
+        'js',
+        'php',
+        'serve'
+    ));
